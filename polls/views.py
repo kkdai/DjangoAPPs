33f448659fe25ffa3d85a65d9c7982bd313814db
Evan Lin <evanslin@gmail.com>
2014-03-31 00:37:19 +0800

    update templates for index and detail

--- polls/views.py
+++ polls/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from polls.models import Poll
+from django.http import Http404
+ 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the poll index.")
+    latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
+    context = {'latest_poll_list': latest_poll_list}
+    return render(request, 'polls/index.html', context) 
  
 def detail(request, poll_id):
-    return HttpResponse("You're looking at poll {id}.".format(id = poll_id))
- 
+    try:
+        poll = Poll.objects.get(pk=poll_id)
+    except Poll.DoesNotExist:
+        raise Http404 
+    return render(request, 'polls/detail.html', {'poll': poll})
+    
 def results(request, poll_id):
     return HttpResponse("You're looking at the results of poll {id}.".format(id = poll_id))
  
