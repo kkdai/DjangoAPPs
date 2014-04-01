c568f8333e8ba5e0d7b2564c900bd30ba0459418
Evan Lin <evanslin@gmail.com>
2014-04-01 23:40:39 +0800

    update for polls 1. Add result, vote, index python code

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from polls.models import Poll
-from django.http import Http404
- 
+from django.http import Http404, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.core.urlresolvers import reverse
+
 # Create your views here.
 def index(request):
     latest_poll_list = Poll.objects.all().order_by('-pub_date')[:5]
@@ -10,14 +12,23 @@ def index(request):
     return render(request, 'polls/index.html', context) 
  
 def detail(request, poll_id):
-    try:
-        poll = Poll.objects.get(pk=poll_id)
-    except Poll.DoesNotExist:
-        raise Http404 
+    poll = get_object_or_404(Poll, pk=poll_id)
     return render(request, 'polls/detail.html', {'poll': poll})
     
 def results(request, poll_id):
-    return HttpResponse("You're looking at the results of poll {id}.".format(id = poll_id))
+    poll = get_object_or_404(Poll, pk=poll_id)
+    return render(request, 'polls/results.html', {'poll': poll})
  
 def vote(request, poll_id):
-    return HttpResponse("You're voting on poll {id}.".format(id = poll_id))
+    p = get_object_or_404(Poll, pk=poll_id)
+    try:
+        selected_choice = p.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoNotExist):
+        return render(request, 'polls/detail.html', {
+            'polls': p,
+            'error_message': "You do not select a choice",
+            })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
